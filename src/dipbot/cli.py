601225f45328b9f31f4d1af9008f9d0f47c9c45c
circius <circius@posteo.de>
2020-04-04 12:01:14 +0100

    modified cli to provide both local and daemon access to the scraper

--- src/dipbot/cli.py
+++ src/dipbot/cli.py
@@ -4,12 +4,17 @@
 """
 from dipbot import scraper, app
 import click
-from dipbot import app
+from dipbot import app, bot
 from dipbot.data_definitions import DipGame
 
 
-@click.command()
+@click.group()
 def cli():
+    pass
+
+
+@cli.command()
+def report():
     game_id = app.get_env_var_checked(app.WEBDIP_ID_ENV_VAR_NAME)
     dipgame = scraper.get_dipgame_checked(game_id)
     try:
@@ -19,3 +24,9 @@ def cli():
         exit()
 
     click.echo(app.announce_overall_game_state(dipgame))
+
+
+@cli.command()
+def daemon():
+    click.echo("initialising daemon")
+    bot.main()
