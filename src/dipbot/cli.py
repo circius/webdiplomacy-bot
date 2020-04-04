468e5fd32b02a38135bdbffe2456805cbf2cf340
circius <circius@posteo.de>
2020-04-04 19:16:42 +0100

    Introduce input validation for cli handling of game ID env var

--- src/dipbot/cli.py
+++ src/dipbot/cli.py
@@ -16,12 +16,16 @@ def cli():
 @cli.command()
 def report():
     game_id = app.get_env_var_checked(app.WEBDIP_ID_ENV_VAR_NAME)
+    try:
+        assert game_id != False
+    except:
+        exit(1)
     dipgame = scraper.get_dipgame_checked(game_id)
     try:
         assert dipgame != False
     except:
         click.echo("exiting...")
-        exit()
+        exit(1)
 
     click.echo(app.announce_overall_game_state(dipgame))
 
