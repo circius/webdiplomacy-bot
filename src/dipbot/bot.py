b033ae9da9358739511959707414414756f6b2de
circius <circius@posteo.de>
2020-04-04 19:15:33 +0100

    Introduce clunky input validation for the bot's environment.

--- src/dipbot/bot.py
+++ src/dipbot/bot.py
@@ -2,11 +2,28 @@ import discord
 import os
 from dipbot import app, scraper
 
-api_token = os.getenv("DISCORD_API_KEY")
-webdip_id = app.get_env_var_checked(app.WEBDIP_ID_ENV_VAR_NAME)
-
 
 def main():
+
+    api_token = app.get_env_var_checked("DISCORD_API_KEY")
+    try:
+        assert api_token != False
+    except:
+        exit(1)
+    game_id = app.get_env_var_checked(app.WEBDIP_ID_ENV_VAR_NAME)
+    try:
+        assert game_id != False
+    except:
+        exit(1)
+    dipgame = scraper.get_dipgame_checked(game_id)
+    try:
+        assert dipgame != False
+    except:
+        print(
+            "Could not get status of game with id {game_id}. Are you sure it's valid? Exiting..."
+        ).format(client)
+        exit(1)
+
     client = discord.Client()
 
     @client.event
