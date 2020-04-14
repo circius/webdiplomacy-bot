ba90d88f34114ad6f89eaf982e585763c0d321b9
circius <circius@posteo.de>
2020-04-14 17:27:47 +0100

    Added code to app to produce output for $status? and $status[!?|?!]

--- src/dipbot/app.py
+++ src/dipbot/app.py
@@ -9,24 +9,55 @@ from typing import List
 
 WEBDIP_ID_ENV_VAR_NAME = "WEBDIP_GAME_ID"
 
+MAIN_PHASE_LENGTH = utilities.get_env_var_checked(
+    "MAIN_PHASE_LENGTH", "an unknown number of days"
+)
 
-def get_env_var_checked(varname: str) -> Union[str, bool]:
-    """gets the value of an environment variable. raises an exception and
-returns False if the variable is not set.
+AUXILIARY_PHASE_LENGTH = utilities.get_env_var_checked(
+    "AUXILIARY_PHASE_LENGTH", "an unknown number of hours"
+)
+
+GAME_PHASE_DESCRIPTIONS = {
+    "diplomacy": f"""This is a normal turn devoted to diplomacy, intrigue, and manoeuvre. In order to complete your turn you have to assign orders to all of your units, then click 'ready'; but you should expect to spend a good deal of time negotiating with enemies and friends about how to proceed. We have agreed to apply orders in this phase within {MAIN_PHASE_LENGTH}; the game will proceed automatically after that time has elapsed. IMPORTANT NOTE: provinces will only change hands if they are occupied *at the end of the year* by an invading power; if I move into an enemy province in the spring, then move out again in the autumn leaving it unoccupied, that province will not change hands.
+""",
+    "retreats": f"""This is the 'retreats' phase, one of the two auxiliary phases which occur at the end of each year. Only those powers who have been defeated and forced to retreat from some province have any orders to give; everyone else is waiting. We have agreed that these auxiliary phases should be completed within {AUXILIARY_PHASE_LENGTH}.
+""",
+    "builds": f"""This is the 'builds' phase, one of the two auxiliary phases which occur at the end of each year. Only those powers who have gained or lost supply centers in the course of the last year will have any orders to give; those who have gained supply centres will be able to build new armies in their core provinces; those who have lost supply centres will have to destroy armies. Everyone else is waiting. We have agreed that these auxiliary phases should be completed within {AUXILIARY_PHASE_LENGTH}.
+""",
+}
 
 
-
-def announce_overall_game_state(state: DipGame) -> str:
+def announce_overall_game_state(state: DipGame, verbose: bool = False) -> str:
     """Consumes a DipGame and produces a string summarising the game
-state.
+state. If verbose is set to True, interpolates a string describing the
+purpose of the game state.
 
     """
 
+    date_announcement = f"It is the {state['season']} of {state['year']}.\n"
+
+    phase = state["phase"]
+
+    if verbose:
+        phase_announcement = phase_get_verbose_description(phase)
+    else:
+        phase_announcement = f"Phase: {phase}.\n"
+
     waiting_for = dipgame_get_uncommitted_players(state)
 
-    return f"""It is the {state['season']} of {state['year']}.
-We are awaiting the orders of:
-{format_the_tardy_list(waiting_for)}"""
+    awaiting_announcement = f"""We are awaiting the orders of:
+{format_the_tardy_list(waiting_for)}
+"""
+    return date_announcement + phase_announcement + awaiting_announcement
+
+
+def phase_get_verbose_description(phase: Phase) -> str:
+    """Consumes a Phase and produces its corresponding verbose
+description.
+
+    """
+
+    return GAME_PHASE_DESCRIPTIONS[phase]
 
 
 def format_the_tardy_list(lop: List[Player]) -> str:
--- tests/test_app.py
+++ tests/test_app.py
@@ -9,6 +9,23 @@ def test_can_accurately_report_overall_game_state():
     assert type(announcement) == str
     assert "1904" in announcement
     assert "Spring" in announcement
+    assert "diplomacy" in announcement
+
+    announcement_retreats = app.announce_overall_game_state(
+        testdata.pop_the_bee_retreats_DipGame
+    )
+    assert type(announcement) == str
+    assert "1905" in announcement_retreats
+    assert "Autumn" in announcement_retreats
+    assert "retreats" in announcement_retreats
+
+    announcement_builds = app.announce_overall_game_state(
+        testdata.pop_the_bee_builds_DipGame
+    )
+    assert type(announcement) == str
+    assert "1905" in announcement_builds
+    assert "Autumn" in announcement_builds
+    assert "builds" in announcement_builds
 
 
 def test_can_get_an_accurate_list_of_uncommitted_players():
@@ -16,3 +33,28 @@ def test_can_get_an_accurate_list_of_uncommitted_players():
     assert type(players) == list
     assert len(players) == 1
     assert players[0]["name"] == "MichiganMan"
+
+
+def test_produces_verbose_descriptions_on_request():
+    announcement = app.announce_overall_game_state(testdata.pop_the_bee_DipGame)
+    verbose_announcement = app.announce_overall_game_state(
+        testdata.pop_the_bee_DipGame, True
+    )
+    assert len(verbose_announcement.split()) > len(announcement)
+
+    announcement_retreats = app.announce_overall_game_state(
+        testdata.pop_the_bee_retreats_DipGame
+    )
+    verbose_announcement_retreats = app.announce_overall_game_state(
+        testdata.pop_the_bee_retreats_DipGame, True
+    )
+
+    assert len(verbose_announcement.split()) > len(announcement_retreats)
+
+    announcement_builds = app.announce_overall_game_state(
+        testdata.pop_the_bee_builds_DipGame
+    )
+    verbose_announcement_builds = app.announce_overall_game_state(
+        testdata.pop_the_bee_builds_DipGame, True
+    )
+    assert len(verbose_announcement.split()) > len(announcement_builds)
