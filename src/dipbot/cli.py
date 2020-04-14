5e87d7ee8f74ae12e23c202cbc00e63f52690646
circius <circius@posteo.de>
2020-04-14 21:02:28 +0100

    Added -v and --verbose flags to dipbot report

--- src/dipbot/cli.py
+++ src/dipbot/cli.py
@@ -14,7 +14,8 @@ def cli():
 
 
 @cli.command()
-def report():
+@click.option("-v", "--verbose", is_flag=True, help="prints verbose output")
+def report(verbose):
     game_id = utilities.get_env_var_checked(app.WEBDIP_ID_ENV_VAR_NAME)
     try:
         assert game_id != False
@@ -27,7 +28,7 @@ def report():
         click.echo("exiting...")
         exit(1)
 
-    click.echo(app.announce_overall_game_state(dipgame))
+    click.echo(app.announce_overall_game_state(dipgame, verbose))
 
 
 @cli.command()
