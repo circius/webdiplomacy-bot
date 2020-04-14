7111234d99b8b8b225cc37190e8fc862440b5ef0
circius <circius@posteo.de>
2020-04-14 20:16:19 +0100

    Removed tricky dependency of get_env_var_checked on python's very flexible booleans

--- src/dipbot/utilities.py
+++ src/dipbot/utilities.py
@@ -7,7 +7,7 @@ import os
 from typing import Union
 
 
-def get_env_var_checked(varname: str, default: str = "") -> Union[str, bool]:
+def get_env_var_checked(varname: str, default: Union[str,bool] = False) -> Union[str, bool]:
     """gets the value of an environment variable. raises an exception and
 returns False if the variable is not set, unless the optional veriable
 default has been set, in which case it returns that instead.
@@ -17,7 +17,7 @@ default has been set, in which case it returns that instead.
     try:
         assert value != None and value != ""
     except AssertionError:
-        if default:
+        if default != False:
             return default
         else:
             print(f"{varname} unset! terminating...")
