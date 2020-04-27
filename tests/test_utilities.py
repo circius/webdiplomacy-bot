033bcb7219465b1323a070c84a2a89455cf081ba
circius <circius@posteo.de>
2020-04-27 13:41:56 +0100

    Moved test for get_env_variable_or_exit

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -17,3 +17,16 @@ def test_get_env_var_checked_can_get_arbitrary_env_var(monkeypatch):
     monkeypatch.setenv("DISCORD_API_KEY", "nonsense")
     key = utilities.get_env_var_checked("DISCORD_API_KEY")
     assert key == "nonsense"
+
+def test_gets_env_variable_or_exits(monkeypatch):
+    monkeypatch.setenv("DISCORD_API_KEY", "nonsense")
+    value = utilities.get_env_var_or_exit("DISCORD_API_KEY")
+    assert value == "nonsense"
+
+    nokey = "NO_SUCH_KEY_WILL_EVER_BE_SET"
+    monkeypatch.delenv(nokey, raising=False)
+
+    # code for testing exits from https://medium.com/python-pandemonium/testing-sys-exit-with-pytest-10c6e5f7726f
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        utilities.get_env_var_or_exit(nokey)
+    assert pytest_wrapped_e.type == SystemExit
