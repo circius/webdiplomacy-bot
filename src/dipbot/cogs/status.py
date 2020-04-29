a91f6044769628734ea733dff3b6faf7415aceec
circius <circius@posteo.de>
2020-04-29 15:46:30 +0100

    As above, plus black reformatting

--- src/dipbot/cogs/status.py
+++ src/dipbot/cogs/status.py
@@ -1,10 +1,11 @@
 from discord.ext import commands
 from dipbot import scraper, app, bot_utilities
 
+
 class Status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.game_id = app.GAME_ID
+        self.game_id = app.get_game_id()
 
     @commands.command()
     async def status(self, ctx):
@@ -21,8 +22,8 @@ class Status(commands.Cog):
 """
         dipgame = scraper.get_dipgame_checked(self.game_id)
         status_message = bot_utilities.create_urgent_message(
-                app.announce_overall_game_state(dipgame), ctx.message.author.name
-            )
+            app.announce_overall_game_state(dipgame), ctx.message.author.name
+        )
         await ctx.message.channel.send(status_message)
 
     @commands.command(name="status?")
@@ -41,6 +42,7 @@ state. useful for beginners.
 """
         dipgame = scraper.get_dipgame_checked(self.game_id)
         status_message = bot_utilities.create_urgent_message(
-            app.announce_overall_game_state(dipgame, verbose=True), ctx.message.author.name
+            app.announce_overall_game_state(dipgame, verbose=True),
+            ctx.message.author.name,
         )
         await ctx.message.channel.send(status_message)
