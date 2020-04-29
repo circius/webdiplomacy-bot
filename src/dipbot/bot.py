a72fc0073b4663c26af4acadb28ceeb2eaf3f08f
circius <circius@posteo.de>
2020-04-29 15:50:32 +0100

    Refactoring to make use of ClientUser.mentioned_in(message) method.

--- src/dipbot/bot.py
+++ src/dipbot/bot.py
@@ -6,21 +6,28 @@ from dipbot import app, scraper, utilities, bot_utilities
 from dipbot.data_definitions import DipGame
 
 COMMAND_PREFIX = "$"
-    
+
+
 class DipBot(commands.Bot):
     def __init__(self):
         super().__init__(COMMAND_PREFIX)
 
+
 BOT = DipBot()
+
 
 @BOT.event
 async def on_ready():
     print("We have logged in as {0.user}".format(BOT))
 
+
 async def on_message(message):
-    if bot_utilities.message_mentions_botP(message, BOT) and bot_utilities.message_is_pleadingP(message):
-        await message.channel.send(f"""I would love to help but I only understand a few messages, always prefixed by `{COMMAND_PREFIX}`. If you ask for `{COMMAND_PREFIX}help` I'll tell you about them.""")
-    
+    if bot_utilities.message_is_pleading_with_clientuserP(message, BOT.user):
+        await message.channel.send(
+            f"""I would love to help but I only understand a few messages, always prefixed by `{COMMAND_PREFIX}`. If you ask for `{COMMAND_PREFIX}help` I'll tell you about them."""
+        )
+
+
 def main():
     API_TOKEN = utilities.get_env_var_or_exit("DISCORD_API_KEY")
     BOT.add_cog(status.Status(BOT))
--- src/dipbot/bot_utilities.py
+++ src/dipbot/bot_utilities.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+
 def create_urgent_message(normal_message: str, invoking_user: str = "") -> str:
     """consumes a string representing a message to be posted to a discord
 channel and produces an urgent version by prepending a string
@@ -17,22 +18,6 @@ set, the urgent message will blame that user.
     return "\n".join([urgent_prefix, normal_message])
 
 
-def message_mentions_botP(message: discord.message, BOT: commands.bot) -> bool:
-    """consumes a discord.py message and a discord BOT and produces true if
-it mentions the bot, false otherwise.
-
-    """
-    return mentions_includes_user(message.mentions, BOT.user)
-
-
-def mentions_includes_user(mentions: list, user: discord.ClientUser) -> bool:
-    """consumes a list of mentions from a discord.py message and produces
-true if one of the users mentioned has the supplied ClientUser, false
-otherwise.
-
-    """
-    return any(map(lambda x: x.name == user.name, mentions))
-
 def message_is_pleadingP(message: discord.Message) -> bool:
     """consumes a discord.py message and returns 'true' if it's a
 pleading message, false otherwise.
@@ -40,3 +25,13 @@ pleading message, false otherwise.
     """
     pleading_lexemes = ["help", "please", "?", "how"]
     return any(map(lambda x: x in message.content, pleading_lexemes))
+
+
+def message_is_pleading_with_clientuserP(
+    message: discord.Message, clientuser: discord.ClientUser
+) -> bool:
+    """consumes a discord message and a discord ClientUser and produces
+true if the message is both "pleading" and mentions the user.
+
+    """
+    return clientuser.mentioned_in(message) and message_is_pleadingP(message)
